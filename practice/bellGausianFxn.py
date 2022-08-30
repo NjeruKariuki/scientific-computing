5693635f457cc15aace92db9b1cbd8e3f0b8edf0
Daniel Kariuki <freakoutbond2@gmail.com>
2022-08-30 12:29:49 +0300

    created function for bellfxn

--- practice/bellGausianFxn.py
+++ practice/bellGausianFxn.py
@@ -5,24 +5,22 @@ import random
 '''
 3 variables, x,s, and m
 
-if x tends from 0 to 100, by 5, put all x into list of x
+if x tends from 0 to 10, by .5, put all x into list
 '''
 all_x = [x/10 for x in range(0, 100, 5)]
 
-''' variables'''
+'''randomized variables for more fun'''
 s = random.randint(0, len(all_x))
-print(s)
 m = random.randint(0, len(all_x))
-print(m)
-results = []
+x = 6
 
+#created function for bell-curve gaussian distribution
+def bellGausianFxn(x):
+    f = (1/(sqrt(2*pi)*s))*exp(-0.5*((x-m)/s)**2)
+    return f
 
-for x in all_x:
-    res = (1/(sqrt(2*pi)*s))*exp(-0.5*((x-m)/s)**2)
-    results.append(res)
-
-for res in results:
-    print(res)
+#get y values
+results = [bellGausianFxn(x) for x in all_x]
 
 #plotting the results
 plt.plot(all_x, results)
